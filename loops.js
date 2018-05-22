3667e89016c8c1b496c491e1cd7ffb7d59f85410
Harry Dibden <harry-dibden@hotmail.com>
2018-05-22 21:02:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,11 +18,11 @@ function whileLoop(num) {
 }
 
 function maybeTrue() {
-    return Math.random() >= 0.5;
-  }
+  return Math.random() >= 0.5;
+}
 
 function doWhileLoop(array){
   do {
     array.shift();
-  } while (array.length > 0 && maybeTrue());
+  } while (array.length >= 0 && maybeTrue());
 }
